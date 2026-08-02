43ac3389888ea8f3d89b736de3516c0cd1a7b2d9
Rimu Eirnarn <Rimuru720@proton.me>
2026-08-02 18:00:19 +0700

    something

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ typedef enum { AT_NORMAL, AT_FOLLOW_UP, AT_ULTIMATE, AT_EXTRA_ACTION } ActionTyp
 
 typedef struct Attributes { float HP; int ATK; int DEF; } Attributes;
 
-struct Entity { char name[20]; Attributes attr; int level; Ability abilities[10]; };
+struct Entity { char name[20]; Attributes attr; int level; Ability abilities[10]; }; // h. Struct, e. Array, g. String
 
 void clear()
 {
@@ -44,72 +44,71 @@ void clear()
 #endif
 }
 
-int attack(Entity *self, Entity *target) {
-    float damage = self->attr.ATK - target->attr.DEF;
-    target->attr.HP -= damage;
+int attack(Entity *self, Entity *target) { // f. Fungsi
+    float damage = self->attr.ATK - target->attr.DEF; // a. Deklarasi, b. inisialisasi
+    target->attr.HP -= damage; // a. Deklarasi
     printf("\n%s deals %.0f damage!\n", self->name, damage);
     delay_ms(ENEMY_ACTION_SLEEP);
     return target->attr.HP <= 0;
 }
 
-int player_basic(Entity *self, Entity *target) {
-    float damage = (self->attr.ATK * 1) - target->attr.DEF;
-    target->attr.HP -= damage;
+int player_basic(Entity *self, Entity *target) { // f. Fungsi
+    float damage = (self->attr.ATK * 1) - target->attr.DEF; // a. Deklarasi, b. Inisialisasi
+    target->attr.HP -= damage; // a. Deklarasi
     printf("%s deals %.0f damage!\n", self->name, damage);
     delay_ms(PLAYER_ACTION_SLEEP);
     return target->attr.HP <= 0;
 }
 
-int player_altba(Entity *self, Entity *target) {
-    float damage = (self->attr.ATK * 1.5) - target->attr.DEF;
-    target->attr.HP -= damage;
+int player_altba(Entity *self, Entity *target) { // f. Fungsi
+    float damage = (self->attr.ATK * 1.5) - target->attr.DEF; // a
+    target->attr.HP -= damage; // a
     printf("%s deals %.0f damage!\n", self->name, damage);
     delay_ms(PLAYER_ACTION_SLEEP);
     return target->attr.HP <= 0;
 }
 
-Ability action_prompt(Entity *player) {
-    while (true) {
-        int index = -1;
+Ability action_prompt(Entity *player) { // f. Fungsi
+    while (true) { // d. Perulangan
+        int index = -1; // a
         printf("1. Basic | 2. Alt Skill | > ");
-        if (scanf("%d", &index) != 1) { while (getchar() != '\n'); continue; };
-        if ((index == 1) || (index == 2)) {
+        if (scanf("%d", &index) != 1) { while (getchar() != '\n'); continue; }; // c. Percabangan
+        if ((index == 1) || (index == 2)) { // c
             return player->abilities[index - 1];
         }
     }
 }
 
-void update(Entity *turn_order[], int turn_index) {
+void update(Entity *turn_order[], int turn_index) { // f. Fungsi
     Entity *attacker = turn_order[turn_index];
     Entity *defender = turn_order[(turn_index == 0) ? 1 : 0];
-    Ability ability;
+    Ability ability; // a
 
-    if (strcmp(attacker->name, turn_order[1]->name) == 0) ability = attack;
-    else ability = action_prompt(attacker);
+    if (strcmp(attacker->name, turn_order[1]->name) == 0) ability = attack; // b, c
+    else ability = action_prompt(attacker); // b, c
 
     ability(attacker, defender);
 }
 
-int main() {
-    Entity player = {"Rimu Aerisya", {250, 20, 8}, 1, {player_basic, player_altba}};
-    Entity bat = {"Bat", {120, 10, 0}, 1, {attack}};
-    Entity *turn_order[2] = {&player, &bat};
-    unsigned int cycle = 1;
-    while (true) {
-        for (size_t i = 0; i < 2; i++)
-        {
+int main() { // f. Fungsi
+    Entity player = {"Rimu Aerisya", {250, 20, 8}, 1, {player_basic, player_altba}}; // a, b
+    Entity bat = {"Bat", {120, 10, 0}, 1, {attack}}; // a, b
+    Entity *turn_order[2] = {&player, &bat}; // a, b
+    unsigned int cycle = 1; // a, b
+    while (true) { // d
+        for (size_t i = 0; i < 2; i++) { // d 
             clear();
             printf("Cycle #%d\n", cycle);
             printf("%-20s [%.0f HP] [%d ATK] [%d DEF]\n", player.name, player.attr.HP, player.attr.ATK, player.attr.DEF);
             printf("%-20s [%.0f HP] [%d ATK] [%d DEF]\n", bat.name, bat.attr.HP, bat.attr.ATK, bat.attr.DEF);
             update(turn_order, i);
-            if (i == 0)cycle++;
-            if (!((player.attr.HP <= 0) || (bat.attr.HP <= 0))) { continue; }
-            Entity e = (player.attr.HP <= 0) ? bat : player;
+            if (i == 0) cycle++; // c
+            if (!((player.attr.HP <= 0) || (bat.attr.HP <= 0))) { continue; } // c
+            Entity e = (player.attr.HP <= 0) ? bat : player; // b, c
             char *name = e.name;
             printf("%s win!\n", name);
             return 0;
         }
     }
-    return 0;
+    return 0;   
 }
